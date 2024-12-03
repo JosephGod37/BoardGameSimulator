f0f7f14e4bac15bec0a011958e37ddac137f7c11
Joseph <Josph@com.pl>
2024-12-03 08:53:20 +0100

    first commit

---
+++ BoardGameSolution/BoardGameSolution/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Player
+{
+    public string Name;
+    public string Position;
+    public int Score;
+
+    public Player()
+    {
+        Name = "Unknown";
+        Position = "Unknown";
+        Score = 0;
+    }
+    public Player(string Name, string Position, int Score)
+    {
+        Name = this.Name;
+        Position = this.Position;
+        Score = this.Score;
+    }
+}
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+       
+    }
+}
